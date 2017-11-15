f09c1b97dace6fb4e43a6b1ac442be512990baaa
Andrei-Tsiushkevich <atemcont@mail.ru>
2017-11-15 18:27:54 +0300

    User input

---
+++ ArgRunner.java
@@ -0,0 +1,18 @@
+/**
+ * Класс для запуска калькулятор с параметрами.
+ */
+public class ArgRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Calculate...");
+		if (args.length == 2) {
+			int first = Integer.valueOf(args[0]);
+			int second = Integer.valueOf(args[1]);
+			Calculator calc = new Calculator();
+			calc.add(first, second);
+			System.out.println("Sum : " + calc.getResult());
+		} else {
+			System.out.println("Enter two paramenters.");
+		}
+	}
+}
